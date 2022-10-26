4771e59262bf8d53f140b235c136abf5ff1871da
Niall <jackniall9@gmail.com>
2022-10-26 17:51:53 +0100

    Cleaned up and customised

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Aerial by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function() {
 
 	skel.init({
@@ -33,30 +27,4 @@
 			window.onorientationchange = function() {
 				document.body.scrollTop = 0;
 			}
-
-	/*
-
-	// Events (jQuery).
-	// Aerial doesn't need jQuery, but if you're going to use it anyway remove the
-	// block of JS events above and use the jQuery-based ones below instead.
-
-		jQuery(window)
-
-			// Remove "loading" class once the page has fully loaded.
-				.on('load', function() {
-					jQuery('body').removeClass('loading');
-				})
-
-			// Prevent scrolling on touch.
-				.on('touchmove', function() {
-					return false;
-				})
-
-			// Fix scroll position on orientation change.
-				.on('orientationchange', function() {
-					jQuery('body').scrollTop(0);
-				});
-
-	*/
-
 })();
